cc0a2b810ed06c57773db5bb5931528133f36aeb
Alvaro Capaceta <ingcapadev@gmail.com>
2023-06-19 14:33:26 -0700

    Fix Image Button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
   return (
     <div className="col-span-2 flex items-center justify-center bg-orange-300">
-      <img src={`/img/${props.img}`} alt="" />
+      <img src={`/img/${props.img}`} alt="" className="px-4" />
     </div>
   );
 };
