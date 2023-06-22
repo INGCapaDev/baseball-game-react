d1fe5847f7be27abea3b31ef5d20bbfc1a0c2b7c
Alvaro Capaceta <ingcapadev@gmail.com>
2023-06-22 07:56:12 -0700

    Font for all elements

--- src/players/components/player/components/Text.jsx
+++ src/players/components/player/components/Text.jsx
@@ -1,6 +1,6 @@
 const Text = (props) => {
   return (
-    <span className="font-sans text-[3.5vh] font-bold leading-[1] text-white">
+    <span className="font-chalk text-[3vh] font-bold leading-[1] text-white">
       {props.text}
     </span>
   );
