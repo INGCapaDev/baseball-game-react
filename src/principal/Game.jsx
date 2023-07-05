4f24eb13f2c06033e20018d2ebb0908421a02bbd
Alvaro Capaceta <ingcapadev@gmail.com>
2023-07-05 09:04:39 -0700

    Button onClick Animation Effect

--- src/principal/Game.jsx
+++ src/principal/Game.jsx
@@ -38,7 +38,9 @@ function Game({ handlePlayersView }) {
       <div className="container-grid grid-fullscreen m-1 grid gap-1">
         <div className="col-span-6 row-span-4 bg-orange-300"></div>
         <Out text="Out"></Out>
+
         <Button img="settings.svg"></Button>
+
         {game ? (
           <Play
             text="Jugada"
--- src/principal/components/play/Play.jsx
+++ src/principal/components/play/Play.jsx
@@ -1,6 +1,13 @@
+import { useRef } from "react";
+import { useClickAnimation } from "../../../hooks/useClickAnimation";
+
 const Play = ({ text, onClick, disabled }) => {
+  const playRef = useRef();
+  useClickAnimation(playRef, {});
+
   return (
     <button
+      ref={playRef}
       className="col-span-4 row-span-2 flex items-center justify-center bg-orange-500"
       onClick={onClick}
       disabled={disabled}
