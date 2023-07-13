950d06874d755e15e259dd2a66cb44c5c43e10dd
Alvaro Capaceta <ingcapadev@gmail.com>
2023-07-13 13:12:42 -0700

    Transition bases background

--- src/principal/components/bases/components/Base.jsx
+++ src/principal/components/bases/components/Base.jsx
@@ -4,7 +4,7 @@ const Base = ({ base }) => {
   return (
     <div
       className={classNames(
-        "h-[40%] w-[40%] border-4 border-white absolute",
+        "h-[40%] w-[40%] border-4 border-white absolute transition-colors duration-200",
         base
       )}
     ></div>
