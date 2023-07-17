106aeb586f8ac914dc61fc6b89032a921832ac74
Alvaro Capaceta <ingcapadev@gmail.com>
2023-07-17 08:54:24 -0700

    Fix at_bat selector in useCheckBalls

--- src/hooks/useCheckBalls.js
+++ src/hooks/useCheckBalls.js
@@ -4,11 +4,17 @@ import { gameSlice } from "../redux/reducers/gameSlice";
 
 const useCheckBalls = () => {
   const dispatch = useDispatch();
-  const { turn, balls, at_bat } = useSelector((state) => state.game);
+  const { turn, balls } = useSelector((state) => state.game);
+  const at_bat = useSelector((state) => state[turn]?.at_bat);
 
   useEffect(() => {
     if (balls === 4) {
-      dispatch(gameSlice.actions.basePerBall({ at_bat, turn }));
+      dispatch(
+        gameSlice.actions.basePerBall({
+          at_bat,
+          turn,
+        })
+      );
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [balls]);
