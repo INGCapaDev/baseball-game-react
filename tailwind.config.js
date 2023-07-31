17f2b2731e79a64a142d914bc3fe88b90772c7ce
Alvaro Capaceta <ingcapadev@gmail.com>
2023-07-31 12:48:40 -0700

    chore: Colors palette config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ export default {
       boxShadow: {
         modal: "inset 0 0 8px 2px rgba(214,148,17,1);",
       },
+      colors: {
+        primary: {
+          DEFAULT: "#ab1217",
+          dark: "#ab1217",
+          light: "#ab1217",
+        },
+        secondary: {
+          DEFAULT: "#000000",
+          dark: "#000000",
+          light: "#000000",
+        },
+        tertiary: {
+          DEFAULT: "#f7b500",
+          dark: "#f7b500",
+          light: "#f7b500",
+        },
+      },
     },
   },
   plugins: [],
