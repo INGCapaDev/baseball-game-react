ed18c4df9edc71ab36fd8c141c349267936eb731
Alvaro Capaceta <ingcapadev@gmail.com>
2023-09-06 13:43:13 -0700

    feat(tour): New design for tour steps and hide navigation controls

--- src/main.jsx
+++ src/main.jsx
@@ -9,24 +9,49 @@ import { TourProvider } from "@reactour/tour";
 const steps = [
   {
     selector: ".first-step",
-    content:
-      "Bienvenido, primero configura el nombre del equipo y los nombres de los jugadores del equipo visitante",
+    content: (
+      <img
+        src="./steps/first-step.jpeg"
+        alt="Bienvenido, primero configura el nombre del equipo y los nombres de los jugadores del equipo visitante"
+      />
+    ),
   },
   {
     selector: ".second-step",
-    content:
-      "Una vez termines de configurar el equipo visitante lo siguiente es hacer los mismo para el equipo local",
+    content: (
+      <img
+        src="./steps/second-step.jpeg"
+        alt="Una vez termines de configurar el equipo visitante lo siguiente es hacer los mismo para el equipo local"
+      />
+    ),
   },
   {
     selector: ".third-step",
-    content:
-      "Al terminar de configurar ambos equipos dale click al boton jugar para comenzar el juego",
+    content: (
+      <img
+        src="./steps/third-step.jpeg"
+        alt="Al terminar de configurar ambos equipos dale click al boton jugar para comenzar el juego"
+      />
+    ),
   },
 ];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <TourProvider steps={steps}>
+    <TourProvider
+      steps={steps}
+      showBadge={false}
+      showNavigation={false}
+      showCloseButton={false}
+      styles={{
+        popover: (base) => ({
+          ...base,
+          borderRadius: 10,
+          padding: 10,
+          backgroundColor: "#1f2d2d",
+        }),
+      }}
+    >
       <AudioContextProvider>
         <App />
       </AudioContextProvider>
